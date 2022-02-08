548affa1d43a8705a1d2cbad6091c3ad725cfdad
KMP <krzysztof.picur@gmail.com>
2022-02-08 22:25:17 +0100

    fixing upload

--- resources/views/Email/passwordReset.blade.php
+++ resources/views/Email/passwordReset.blade.php
@@ -4,8 +4,8 @@
 Aby zresetować swoje hasło w sklepie BASE-PC kliknij w przycisk znajdujący
 się poniżej.
 
-@component('mail::button', ['url' =>'http://localhost:8080/#/home/password_reset?token='.$token])
-        Resetuj hasło
+@component('mail::button', ['url' =>'https://basepc.online/#/home/password_reset?token='.$token])
+Resetuj hasło
 @endcomponent
 
 Dziękujemy i życzymy udanych zakupów<br>
